d858116c51557c591ea1f9b10d4c7d37dd8fb20d
NazarBunko <bunkonazar@gmail.com>
2024-11-13 19:50:42 +0200

    added new strategies

--- pizza/src/main/java/kpp/project/pizza/models/strategies/RandomStrategy.java
+++ pizza/src/main/java/kpp/project/pizza/models/strategies/RandomStrategy.java
@@ -8,16 +8,9 @@ public class RandomStrategy implements IPizzaStrategy {
     private Random random = new Random();
 
     @Override
-    public Pizzeria createPizzeria() {
-        // Логіка для створення піцерії з випадковими налаштуваннями
-        System.out.println("Створення піцерії з випадковими налаштуваннями.");
-        return null; // Повертає новий об'єкт Pizzeria
-    }
-
-    @Override
-    public void generateDelays(Pizzeria pizzeria) {
+    public int generateDelays() {
         // Логіка для генерації випадкових затримок
         int delay = random.nextInt(15); // Генерує випадкову затримку до 15
-        System.out.println("Генерація випадкової затримки: " + delay + " у піцерії.");
+        return delay;
     }
 }
