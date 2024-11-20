003ec662b5fd1fbd9c8f49239674a21f64eb3bda
Stanislav3423 <svolk9954@gmail.com>
2024-11-20 15:57:23 +0200

    Pizzas Statuses Controller

---
+++ pizza/src/main/java/kpp/project/pizza/controllers/PizzaStatusController.java
@@ -0,0 +1,30 @@
+package kpp.project.pizza.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kpp.project.pizza.models.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/pizza-status")
+@CrossOrigin(origins = "http://localhost:3000")
+public class PizzaStatusController {
+    // sendpizzaDtoData(new PizzaDataDTO(cooker.getPizza().getState().getClass().getName(),cooker.getPizza().getName(),cooker.getPizza().getOrderId()));
+    @GetMapping
+    public ResponseEntity<List<PizzaDataDTO>> getAllPizzaStatusRequest() {
+        /*Queue<Pizza> pizzas = Kitchen.getPizzas();
+        List<Pizza> pizzaList = pizzas.stream().toList();
+        List<PizzaDataDTO> items = pizzaList.stream()
+                .map(pizza -> new PizzaDataDTO(pizza.getState().getClass().getName(), pizza.getName(), pizza.getOrderId()))
+                .collect(Collectors.toList());*/
+        List<Order> orders = Pizzeria.getInstance().getOrders();
+        List<PizzaDataDTO> items = orders.stream()
+                .flatMap(order -> order.getPizzas().stream())
+                .map(pizza ->  new PizzaDataDTO(pizza.getState().getClass().getName(), pizza.getName(), pizza.getOrderId()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(items);
+    }
+}
