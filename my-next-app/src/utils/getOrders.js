e53ff7097751be0648b5230aa6c0c7e28c30695f
Vladyslav Antonchyk <109813312+BlaSSSTTT@users.noreply.github.com>
2024-11-25 21:51:21 +0200

    getOrder fix

--- my-next-app/src/utils/getOrders.js
+++ my-next-app/src/utils/getOrders.js
@@ -3,7 +3,11 @@ export async function fetchOrders (setOrders){
       const response = await fetch('http://localhost:8080/orders');
       if (response.ok) {
         const data = await response.json();
-        setOrders(data);
+        const dataWithIds = data.map((item, index) => ({
+          ...item, // Зберігаємо існуючі поля
+          id: index + 1 // Додаємо нове поле id
+        }));
+        setOrders(dataWithIds);
       } else {
         console.error('Не вдалося завантажити замовлення');
       }
